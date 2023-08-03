b36813c41ea8eb5c41e74ff83e34968cf88a1633
vicma <vmaldaraca@gmail.com>
2023-08-02 19:59:11 -0600

    Install vanta and add coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import HALO from 'vanta/dist/vanta.halo.min';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as THREE from 'three';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        HALO({
+          el: myRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          amplitudeFactor: 1.70,
+          size: 2.00,
+        }),
+      );
+    }
+
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative px-4" ref={myRef}>
+      <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30" />
+      <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10">Comforfit, muy pronto</h1>
+      <p className="text-white text-xl md:text-2xl">
+        Estamos trabajando duro para traerte algo increíble. ¡Manténganse al tanto!
+      </p>
     </div>
   );
 }
