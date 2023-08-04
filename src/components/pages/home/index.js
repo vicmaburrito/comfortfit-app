f4c3c8171e11c1c525516d547e88fce2a8131467
vicma <vmaldaraca@gmail.com>
2023-08-03 22:33:25 -0600

    Update First block

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import FirstBlock from '../../FirstBlock';
+import Navbar from '../../Navbar';
+import SecondBlock from '../../SecondBlock';
 
 export default function Home() {
   return (
-    <FirstBlock />
+    <>
+      <Navbar />
+      <FirstBlock />
+    </>
   );
 }
